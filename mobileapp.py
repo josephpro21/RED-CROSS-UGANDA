b7b30ef068a75f0f3419fcef5b7f6b2f62bfe969
Josephpro21 <elasujoseph46@gmail.com>
2021-04-07 11:28:08 +0300

    My first step that i took when building the app for Red Cross Uganda

---
+++ mobileapp.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+from kivy.uix.button import Button
+
+class MyApp(App):
+    def build(self):
+            return Button(text = 'REDCROSS UGANDA')
+        
+        
+        
+if __name__ =='__main__':
+             MyApp().run()
